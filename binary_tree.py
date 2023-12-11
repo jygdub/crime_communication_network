595cb75d7c44efea06cf93fc221872cc5a4587f9
jygdub <jade.dubbeld@gmail.com>
2023-12-11 12:24:42 +0100

    network dynamics - forward kingpin's messages

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,12 @@
+"""
+Script for preliminary network of message passing until consensus in a binary tree graph
+
+Written by Jade Dubbeld
+07/12/2023
+"""
+
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -30,9 +38,57 @@ def visualize(G):
     nx.draw(G, pos, labels=nx.get_node_attributes(G,'state'))
     plt.show()
 
+def forward_message(G,index,total_messages):
+    """
+    Function to forward root node's message throughout the entire network.
+    Every downstream node copies root node's message into position.
+
+    """
+
+    message = G.nodes[0]['state'][index]
+    
+    # forward root node's message to all downstream neighbors
+    for node in G.nodes:
+        for neighbor in G.neighbors(node):
+
+            # only propagate downstream
+            if neighbor < node:
+                continue
+
+            # get current state of selected neighbor
+            current_state = G.nodes[neighbor]['state']
+
+            # copy received bit at given position (redundant if bit is already agreed)
+            new_state = current_state[:index] + message + current_state[index + 1:]
+            G.nodes[neighbor]['state'] = new_state
+            total_messages += 1
+    
+    return total_messages
+
+
 if __name__ == '__main__':
     # initialize binary tree
     G = init_binary(depth=3)
 
     # visualize initial state of tree
-    visualize(G)
+    # visualize(G)
+
+    # initial settings
+    total_messages = 0
+    attributes = nx.get_node_attributes(G, "state")
+
+    # converge when all nodes agree states
+    while np.unique(list(attributes.values())).size > 1:
+
+        # pick random bit from state
+        index = random.choice([0,1,2])
+        print(index)
+
+        # propagate root node's message through network
+        total_messages = forward_message(G=G, index=index, total_messages=total_messages)
+
+        # update attributes dictionary
+        attributes = nx.get_node_attributes(G, "state")
+        # visualize(G)
+
+    print(f"Number of messages send until consensus = {total_messages}")
