e2fcdab2d0594d90183a452ea955bd2d644271b8
jygdub <jade.dubbeld@gmail.com>
2024-01-04 21:52:19 +0100

    combined convergence plot

--- lattice-structure/multi_simulate_lattice.py
+++ lattice-structure/multi_simulate_lattice.py
@@ -35,59 +35,73 @@ from lattice import simulate
 # initials
 dimensions = (5,5)
 M = 0
-# alpha = 1.0
-beta = 0.05
 n_iters = 10
 
-for alpha in [1.0, 0.75, 0.5]:
-    # for beta in [0.0, 0.25, 0.5]:
+for beta in [0.0, 0.05]:
 
-    start = timeit.default_timer()
+    combi = plt.figure()
 
-    # simulate until consensus in network
-    total_messages, all_diffScores = simulate(dimensions,alpha,beta,n_iters)
+    for alpha in [1.0, 0.75, 0.5]:
 
-    stop = timeit.default_timer()
-    execution_time = stop - start
+        start = timeit.default_timer()
 
-    print(f"execution time = {execution_time} in seconds")
-    print(f"total messages = {total_messages}")
+        # simulate until consensus in network
+        total_messages, all_diffScores = simulate(dimensions,alpha,beta,n_iters)
 
-    # scatterplot all simulations
-    fig = plt.figure()
-    plt.scatter(range(n_iters),total_messages)
-    plt.xlabel("Iterations", fontsize=14)
-    plt.ylabel("Total messages sent", fontsize=14)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    plt.title(f"Lattice graph (dimensions={dimensions}; N={n_iters}; runtime={round(execution_time,5)} sec.)")
-    plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-simulate{n_iters}-lattice.png"
-                , bbox_inches='tight')
+        stop = timeit.default_timer()
+        execution_time = stop - start
 
-    # convergence plot all simulations
-    fig = plt.figure()
+        print(f"execution time = {execution_time} in seconds")
+        print(f"total messages = {total_messages}")
 
-    for iter in range(n_iters):
-        plt.plot(np.arange(len(all_diffScores[iter]))+1, all_diffScores[iter],label=f"Run{iter}")
+        # scatterplot all simulations
+        fig = plt.figure()
+        plt.scatter(range(n_iters),total_messages)
+        plt.xlabel("Iterations", fontsize=14)
+        plt.ylabel("Total messages sent", fontsize=14)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.title(f"Lattice graph (dimensions={dimensions}; N={n_iters}; runtime={round(execution_time,5)} sec.)")
+        plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-simulate{n_iters}-lattice.png"
+                    , bbox_inches='tight')
+
+        # convergence plot all simulations
+        fig = plt.figure()
+
+        for iter in range(n_iters):
+            plt.plot(np.arange(len(all_diffScores[iter]))+1, all_diffScores[iter],label=f"Run{iter}")
+
+        plt.xlabel("Total messages sent", fontsize=14)
+        plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.title(f"Convergence per run on lattice {dimensions}; alpha={alpha}; beta={beta} (N={n_iters})")
+        plt.legend()
+        plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-convergence{n_iters}-lattice.png")
+
+        # mean convergence plot over all simulations
+        fig = plt.figure()
+        y, error = tolerant_mean(all_diffScores)
+        plt.plot(np.arange(len(y))+1, y, label=f"Mean of {n_iters} iterations")
+        plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5, label=f"Std. dv. of {n_iters} runs")
+
+        plt.xlabel("Total messages sent", fontsize=14)
+        plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.title(f"Mean convergence on lattice {dimensions}; alpha={alpha}; beta={beta} (N={n_iters})")
+        plt.legend()
+        plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-average-convergence{n_iters}-lattice.png")
+
+        # combi plot of convergence for all parameter (alpha/beta) combinations
+        plt.figure(combi)
+        plt.plot(np.arange(len(y))+1, y, label=f"Mean - alpha={alpha}")
+        plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5, label=f"Std. dv. - alpha={alpha}")
 
     plt.xlabel("Total messages sent", fontsize=14)
     plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
-    plt.title(f"Convergence per run on lattice {dimensions}; alpha={alpha}; beta={beta} (N={n_iters})")
+    plt.title(f"Mean convergence on lattice {dimensions} for beta={beta} (N={n_iters})")
     plt.legend()
-    plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-convergence{n_iters}-lattice.png")
-
-    # mean convergence plot over all simulations
-    fig = plt.figure()
-    y, error = tolerant_mean(all_diffScores)
-    plt.plot(np.arange(len(y))+1, y, label=f"Mean of {n_iters} iterations")
-    plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5, label=f"Std. dv. of {n_iters} runs")
-
-    plt.xlabel("Total messages sent", fontsize=14)
-    plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
-    plt.xticks(fontsize=14)
-    plt.yticks(fontsize=14)
-    plt.title(f"Mean convergence on lattice {dimensions}; alpha={alpha}; beta={beta} (N={n_iters})")
-    plt.legend()
-    plt.savefig(f"images/alpha={alpha}-beta={beta}-dim={dimensions}-average-convergence{n_iters}-lattice.png")
+    plt.savefig(f"images/combiplot-beta={beta}-dim={dimensions}-average-convergence{n_iters}-lattice.png")
