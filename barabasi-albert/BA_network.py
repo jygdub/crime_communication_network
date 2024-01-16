d3d3a0ceb0a075cf112c9433eed19729237dc476
jygdub <jade.dubbeld@gmail.com>
2024-01-16 21:45:52 +0100

    simulate message passing framework on barabasi-albert network

---
+++ barabasi-albert/BA_network.py
@@ -0,0 +1,198 @@
+"""
+Script for preliminary network of message passing until consensus in randomly generated Barabasi-Albert networks
+    Simulation for given number of iterations and alpha and beta noise parameter settings
+
+Written by Jade Dubbeld
+16/01/2024
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import random, numpy as np
+
+
+def init_BA(n,m):
+    """
+    Function to generate a random Barabasi-Albert network (a.k.a. scale-free network).
+
+    Parameters:
+    - n: number of nodes in network
+    - m: number of edges to add between new node and existing nodes in network
+
+    Returns:
+    - G: Randomly generated Barabasi-Albert network with given parameters.
+
+    """
+
+    G = nx.barabasi_albert_graph(n,m)
+
+    # initialize all nodes with 3-bit string state
+    for node in G.nodes:
+        binary_string = f'{random.getrandbits(3):=03b}'     # generate random 3-bit string
+        G.nodes[node]['state'] = binary_string
+
+    return G
+
+def visualize(G):
+    """
+    Function to visualize a given graph/network G.
+    Each node is labeled with its current state.
+
+    Parameters:
+    - G: networkx graph to display
+    """
+
+    pos = nx.nx_agraph.graphviz_layout(G, prog="dot")
+
+    nx.draw(G, pos, labels=nx.get_node_attributes(G,'state'))
+    plt.show()
+
+
+def message(G, source, destination, alpha=1.0, beta=0.0):
+    """
+    Function to send some message from source to destination.
+    Correctness of message depends on probability alpha (1.0 is always correct - 0.0 is never correct).
+    Receiver bias depends on probability beta (0.0 is never mistaken - 1.0 is always mistaken).
+
+    Parameters:
+    - G: current state of networkx graph
+    - source: selected sender node in network
+    - destination: selected receiver node in network
+    - alpha: probability of sender bias (sending match or mismatch bits)
+    - beta: probability of receiver bias (flipping message or not)
+
+    Returns:
+    - G: new state of networkx graph
+
+    """
+
+    match = []
+
+    # find correct bits in source node's state compared to destination node's state
+    for position, bit in enumerate(G.nodes[source]['state']):
+        if bit == G.nodes[destination]['state'][position]:
+            match.append(position)
+
+    mismatch = [i for i in [0,1,2] if i not in match]
+
+    # generate random float representing matching/sender bias
+    P_Matching = random.random()
+
+    # random pick if no bits in common
+    # pick from correct list with probability alpha (incorrect with probability 1 - alpha)
+    if P_Matching > alpha and match != []:
+        index = random.choice(match)
+    elif P_Matching <= alpha and mismatch != []:
+        index = random.choice(mismatch)
+    else:
+        index = random.choice([0,1,2])
+
+    # generate message
+    message = G.nodes[source]['state'][index]
+
+    # print(f"index = {index}, message = {message}")
+
+    # generate random float representing miscommunication/receiver bias
+    P_Miss = random.random()
+
+    # copy message given probability beta, otherwise bitflip
+    if P_Miss <= beta:
+        if message == '0':
+            message = '1'
+        elif message == '1':
+            message = '0'
+
+    # get current state of selected downstream neighbor
+    current_state = G.nodes[destination]['state']
+
+    # copy received bit at given position (redundant if bit is already agreed)
+    new_state = current_state[:index] + message + current_state[index + 1:]
+    G.nodes[destination]['state'] = new_state
+
+    return G
+
+
+def hamming_distance(string1, string2):
+    """
+    Function to compute string similarity using Hamming distance.
+
+    Parameters:
+    - string1: First string in comparison
+    - string2: Second string in comparison
+
+    Returns:
+    - distance: number differing characters between string1 and string2
+    """
+
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(string1)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if string1[i] != string2[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+
+def simulate(n, m, alpha=1.0, beta=0.0, n_iters=10):
+    """
+    Function to run a simulation for n_iters on a lattice.
+
+    Parameters:
+    - n: number of nodes in network
+    - m: number of edges to add between new node and existing nodes in network
+    - alpha: probability of sender bias (sending match or mismatch bits)
+    - beta: probability of receiver bias (flipping message or not)
+    - n_iters: number of iterations to simulate for
+
+    Returns:
+    - total_messages: list of total messages sent in all simulations
+    - all_diffScores: list of all string difference scores in all simulations
+    """
+
+    # initials
+    total_messages = []
+    all_diffScores = []
+
+    # simulate for n_iters
+    for iteration in range(n_iters):
+        print(f"iter={iteration}")
+        similarity = []
+
+        G = init_BA(n,m)
+        M = 0
+        attributes = nx.get_node_attributes(G, "state")
+
+        # converge when all nodes agree on state
+        while np.unique(list(attributes.values())).size > 1 or M > 500000:
+            
+            S = []
+
+            source = random.choice(list(G.nodes))
+            destination = random.choice(list(G.neighbors(source)))
+            # print(f"{source} -> {destination}")
+
+            G = message(G=G,source=source,destination=destination,alpha=alpha,beta=beta)
+
+            # print(G.nodes(data=True))
+
+            M += 1
+            attributes = nx.get_node_attributes(G, "state")   
+
+            # string similarity using Hamming distance    
+            for node1 in G.nodes():
+                for node2 in G.nodes():
+                    if node1 == node2:
+                        continue
+
+                    distance = hamming_distance(attributes[node1],attributes[node2])
+                    S.append(distance)
+
+            similarity.append(np.mean(S))
+
+        total_messages.append(M)
+        all_diffScores.append(list(similarity))
+    
+    return total_messages, all_diffScores
---
+++ barabasi-albert/simulate_BA.py
@@ -0,0 +1,116 @@
+"""
+Script for preliminary network of message passing until consensus in randomly generated Barabasi-Albert networks.
+
+Using scalable noise parameters alpha and beta for sender bias and receiver bias, respectively.
+- High alpha means higher probability of sending information the receiver does NOT know yet; 
+    low alpha means higher probability of sending information the receiver ALREADY knows, prolonging the convergence.
+- Low beta means lower probability of no receiving error and therefore no change in information;
+    high beta means higher probability of interpreting message incorrectly and therefore change in information.
+
+Written by Jade Dubbeld
+16/01/2024
+"""
+
+def tolerant_mean(arrs):
+    """
+    Function to compute mean of curves with differing lengths
+
+    Parameters:
+    - arrs: nested list with sublists of different lengths
+
+    Returns:
+    - arr.mean: computed mean over all sublists
+    - arr.std: computed standard deviation over all sublists
+    """
+    lens = [len(i) for i in arrs]
+    arr = np.ma.empty((np.max(lens),len(arrs)))
+    arr.mask = True
+    for idx, l in enumerate(arrs):
+        arr[:len(l),idx] = l
+    return arr.mean(axis = -1), arr.std(axis=-1)
+
+
+
+import timeit, numpy as np, random
+import matplotlib.pyplot as plt
+
+from BA_network import simulate
+
+# initials
+n = 25
+m = 1
+alpha = 1.0
+beta = 0.0
+n_iters = 10
+
+combi = plt.figure()
+
+for m in [1,2,3,4]:
+    
+    print(f"m = {m}")
+
+    start = timeit.default_timer()
+
+    # simulate until consensus in network
+    total_messages, all_diffScores = simulate(n,m,alpha,beta,n_iters)
+
+    stop = timeit.default_timer()
+    execution_time = stop - start
+
+    print(f"execution time = {execution_time} in seconds")
+    print(f"total messages = {total_messages}")
+
+    # scatterplot all simulations
+    fig = plt.figure()
+    plt.scatter(range(n_iters),total_messages)
+    plt.xlabel("Iterations", fontsize=14)
+    plt.ylabel("Total messages sent", fontsize=14)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.title(f"BA network (n={n}; m={m}; n_samples={n_iters}; runtime={round(execution_time,5)} sec.)")
+    plt.savefig(f"images/alpha={alpha}-beta={beta}-n={n}-m={m}-simulate{n_iters}-BA.png",
+                bbox_inches='tight')
+
+    # convergence plot all simulations
+    fig = plt.figure()
+
+    for iter in range(n_iters):
+        plt.plot(np.arange(len(all_diffScores[iter]))+1, all_diffScores[iter],label=f"Run{iter}")
+
+    plt.xlabel("Total messages sent", fontsize=14)
+    plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.title(f"Convergence per run on BA network - n={n}|m={m} (n_samples={n_iters})")
+    plt.legend(bbox_to_anchor=(1,1))
+    plt.savefig(f"images/alpha={alpha}-beta={beta}-n={n}-m={m}-convergence{n_iters}-BA.png"
+                , bbox_inches='tight')
+
+    # mean convergence plot over all simulations
+    fig = plt.figure()
+    y, error = tolerant_mean(all_diffScores)
+    plt.plot(np.arange(len(y))+1, y, label=f"Mean of {n_iters} iterations")
+    plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5, label=f"Std. dv. of {n_iters} runs")
+
+    plt.xlabel("Total messages sent", fontsize=14)
+    plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.title(f"Mean convergence on BA network - n={n}|m={m} (n_samples={n_iters})")
+    plt.legend()
+    plt.savefig(f"images/alpha={alpha}-beta={beta}-n={n}-m={m}-average-convergence{n_iters}-BA.png",
+                bbox_inches='tight')
+
+    # combi plot of convergence for all combinations of BA parameters n, m
+    plt.figure(combi)
+    plt.plot(np.arange(len(y))+1, y, label=f"Mean - m={m}")
+    plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5, label=f"Std. dv. - alpha={alpha}")
+
+plt.xlabel("Total messages sent", fontsize=14)
+plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.title(f"Mean convergence BA network (n={n}, alpha={alpha}, beta={beta}, n_samples={n_iters})")
+plt.legend(bbox_to_anchor=(1,1))
+plt.savefig(f"images/combiplot-alpha={alpha}-beta={beta}-n={n}-average-convergence{n_iters}-BA.png",
+            bbox_inches='tight')
