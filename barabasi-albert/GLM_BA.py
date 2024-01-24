269295287cd7723cd702e4cefc7154b02d44f3f5
jygdub <jade.dubbeld@gmail.com>
2024-01-24 22:55:39 +0100

    run linear regression

---
+++ barabasi-albert/GLM_BA.py
@@ -0,0 +1,67 @@
+"""
+Script to correlate structural efficiency measures with operational efficiency measure.
+
+Written by Jade Dubbeld
+24/01/2024
+"""
+
+import pickle, numpy as np, matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, LogisticRegression
+
+first = 1
+last = 20
+column = 0
+measures = {0: "Link density", 
+            1: "Degree centrality", 
+            2: "Betweenness centrality",
+            3: "Closeness centrality",
+            4: "Clustering coefficient",
+            5: "Transitivity",
+            6: "Global efficiency",
+            7: "Local efficiency"}
+
+path = f"images/efficiency/various-graphs{first}-{last}"
+
+# load consensus formation for all runs on
+consensus_m1 = np.array(pickle.load(open(f"{path}/simulation/consensus-rate-graphs{first}-{last}-alpha=1.0-beta=0.0-n=100-m=1-BA.pickle",'rb')))
+consensus_m2 = np.array(pickle.load(open(f"{path}/simulation/consensus-rate-graphs{first}-{last}-alpha=1.0-beta=0.0-n=100-m=2-BA.pickle",'rb')))
+consensus_m3 = np.array(pickle.load(open(f"{path}/simulation/consensus-rate-graphs{first}-{last}-alpha=1.0-beta=0.0-n=100-m=3-BA.pickle",'rb')))
+consensus_m4 = np.array(pickle.load(open(f"{path}/simulation/consensus-rate-graphs{first}-{last}-alpha=1.0-beta=0.0-n=100-m=4-BA.pickle",'rb')))
+
+# load structural efficiency measures for all graphs
+structural_m1 = pickle.load(open("graphs/m=1/measures-m=1.pickle",'rb'))
+structural_m2 = pickle.load(open("graphs/m=2/measures-m=2.pickle",'rb'))
+structural_m3 = pickle.load(open("graphs/m=3/measures-m=3.pickle",'rb'))
+structural_m4 = pickle.load(open("graphs/m=4/measures-m=4.pickle",'rb'))
+
+X_data = np.append(structural_m1[first-1:last,column].reshape(-1,1), structural_m2[first-1:last,column].reshape(-1,1), axis=0)
+X_data = np.append(X_data, structural_m3[first-1:last,column].reshape(-1,1), axis=0)
+X_data = np.append(X_data, structural_m4[first-1:last,column].reshape(-1,1), axis=0)
+
+Y_data = np.append(consensus_m1, consensus_m2)
+Y_data = np.append(Y_data, consensus_m3)
+Y_data = np.append(Y_data, consensus_m4)
+
+start = 0          # change if necessary (leave out m=1 -> set to 20; else set to 0)
+
+for i in range(1,5):    # change start of range (leave out m=1 -> set to 2; else set to 1)
+    plt.scatter(X_data[0+start:20+start],Y_data[0+start:20+start],label=f"m={i}")
+    start += 20
+
+start = 0          # change if necessary (leave out m=1 -> set to 20; else set to 0)
+
+LinReg = LinearRegression().fit(X_data[start:],Y_data[start:])
+
+x_test = np.linspace(min(X_data[start:]),max(X_data[start:]),100)
+# predict dummy y_test data based on the logistic model
+y_test = x_test * LinReg.coef_ + LinReg.intercept_
+# plt.figure(fig)
+plt.plot(x_test,y_test,'k--',label=f"linear regression \n(coeff={round(LinReg.coef_[0])}; intercept={round(LinReg.intercept_)})")
+plt.xlabel(f"{measures[column]}")
+plt.ylabel("Total of messages until consensus")
+plt.ylim(0)
+plt.legend(bbox_to_anchor=(1,1))
+plt.title(f"Relation between consensus formation and {measures[column].lower()}")
+plt.savefig(f"{path}/linear-regression/link-efficiency-graphs1-20-m=[1,2,3,4]-alpha=1.0-beta=0.0-n=100-BA.png",
+            bbox_inches='tight')
+
