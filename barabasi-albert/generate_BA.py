73ff70a7811b42d94cd40c111b6d0c2b0d252205
jygdub <jade.dubbeld@gmail.com>
2024-02-07 11:01:33 +0100

    adjust to generate for n=1000

--- barabasi-albert/generate_BA.py
+++ barabasi-albert/generate_BA.py
@@ -9,15 +9,16 @@ Written by Jade Dubbeld
 from BA_network import generate
 
 import networkx as nx, numpy as np, pickle
+from tqdm import tqdm
 
-n = 100
-m = 4
+n = 1000
 
-for i in range(1,51):
-    G = generate(n,m)
+for m in range(1,5):
+    for i in tqdm(range(1,1001)):
+        G = generate(n,m)
 
-    pickle.dump(G, open(f'graphs/m={m}/graph{i}-n={n}-m={m}.pickle', 'wb')) 
-        # load using G = pickle.load(open(f'graphs/test.pickle', 'rb'))
+        pickle.dump(G, open(f'graphs/n={n}/m={m}/graph{i}-n={n}-m={m}.pickle', 'wb')) 
+            # load using G = pickle.load(open(f'graphs/test.pickle', 'rb'))
 
 
 
