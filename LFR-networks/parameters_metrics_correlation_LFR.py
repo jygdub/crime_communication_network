9b75de0ee5ca5f759f769562d214b24ea1fe21c6
jygdub <jade.dubbeld@gmail.com>
2024-02-13 13:49:43 +0100

    add (3D) plots to show relation between parameters and measures

--- LFR-networks/parameters_metrics_correlation_LFR.py
+++ LFR-networks/parameters_metrics_correlation_LFR.py
@@ -6,7 +6,6 @@ Written by Jade Dubbeld
 """
 
 import networkx as nx, numpy as np, matplotlib.pyplot as plt, pandas as pd, pickle
-import statsmodels.formula.api as smf 
 
 data = pd.read_csv('data/all-measures.tsv',sep='\t')
 print(data)
@@ -22,11 +21,25 @@ avg_degBetweenness = plt.figure()
 avg_degCFBetweenness = plt.figure()
 avg_degCloseness = plt.figure()
 
+clustering_currentFlow = plt.figure()
+clustering_degree = plt.figure()
+degree_currentFlow = plt.figure()
+
 """Uncomment to initialize 3D plot"""
 # degree = plt.figure()
-# ax = plt.axes(projection ="3d")
+# ax_degree = plt.axes(projection ="3d")
+
+# betweenness = plt.figure()
+# ax_betweenness = plt.axes(projection ="3d")
+
+# CFbetweenness = plt.figure()
+# ax_CFbetweenness = plt.axes(projection ="3d")
+
+# closeness = plt.figure()
+# ax_closeness = plt.axes(projection ="3d")
+
 # clustering = plt.figure()
-# ax = plt.axes(projection ="3d")
+# ax_clustering = plt.axes(projection ="3d")
 """Uncomment until here"""
 
 cm = ['chartreuse','teal','royalblue','deeppink','aquamarine','black','firebrick','darkkhaki','chocolate','forestgreen','orange','darkviolet','red','deepskyblue','fuchsia']
@@ -39,41 +52,59 @@ for avg_deg in [5,10,15,20,25]:
         subset = data.iloc[indices]
 
         plt.figure(muDegree)
-        plt.scatter(subset.mu,subset.degreeCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.mu,subset.degreeCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(muBetweenness)
-        plt.scatter(subset.mu,subset.betweennessCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.mu,subset.betweennessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(muCFBetweenness)
-        plt.scatter(subset.mu,subset.currentFlowBetweenness,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.mu,subset.currentFlowBetweenness,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(muCloseness)
-        plt.scatter(subset.mu,subset.closenessCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.mu,subset.closenessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(muClustering)
-        plt.scatter(subset.mu,subset.clusteringCoefficient,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.mu,subset.clusteringCoefficient,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(avg_degDegree)
-        plt.scatter(subset.average_degree,subset.degreeCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.average_degree,subset.degreeCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(avg_degBetweenness)
-        plt.scatter(subset.average_degree,subset.betweennessCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.average_degree,subset.betweennessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(avg_degCFBetweenness)
-        plt.scatter(subset.average_degree,subset.currentFlowBetweenness,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.average_degree,subset.currentFlowBetweenness,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(avg_degCloseness)
-        plt.scatter(subset.average_degree,subset.closenessCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.average_degree,subset.closenessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         plt.figure(avg_degClustering)
-        plt.scatter(subset.average_degree,subset.clusteringCoefficient,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        plt.scatter(subset.average_degree,subset.clusteringCoefficient,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        plt.figure(clustering_currentFlow)
+        plt.scatter(subset.currentFlowBetweenness,subset.clusteringCoefficient,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        plt.figure(clustering_degree)
+        plt.scatter(subset.degreeCentrality,subset.clusteringCoefficient,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        plt.figure(degree_currentFlow)
+        plt.scatter(subset.currentFlowBetweenness,subset.degreeCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         """Uncomment to plt in 3D"""
         # plt.figure(degree)
-        # ax.scatter3D(subset.mu,subset.average_degree,subset.degreeCentrality,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        # ax_degree.scatter3D(subset.mu,subset.average_degree,subset.degreeCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        # plt.figure(betweenness)
+        # ax_betweenness.scatter3D(subset.mu,subset.average_degree,subset.betweennessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        # plt.figure(CFbetweenness)
+        # ax_CFbetweenness.scatter3D(subset.mu,subset.average_degree,subset.currentFlowBetweenness,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
+
+        # plt.figure(closeness)
+        # ax_closeness.scatter3D(subset.mu,subset.average_degree,subset.closenessCentrality,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
 
         # plt.figure(clustering)
-        # ax.scatter3D(subset.mu,subset.average_degree,subset.clusteringCoefficient,color=cm[count],label=f'avg_deg={avg_deg} & mu={mu}')
+        # ax_clustering.scatter3D(subset.mu,subset.average_degree,subset.clusteringCoefficient,color=cm[count],label=fr'$\langle k \rangle = {avg_deg}, \mu = {mu}$')
         """Uncomment until heere"""
 
         count += 1
@@ -158,23 +189,63 @@ plt.legend(bbox_to_anchor=(1,1))
 plt.tight_layout()
 plt.savefig(f"images/avg_deg-clustering.png",bbox_inches='tight')
 
+plt.figure(clustering_currentFlow)
+plt.xlabel('Current flow betweenness')
+plt.ylabel('Clustering coefficient')
+plt.title('Clustering coefficient against current flow betweenness')
+plt.legend(bbox_to_anchor=(1,1))
+plt.tight_layout()
+plt.savefig(f"images/CFbetweenness-clustering.png",bbox_inches='tight')
+
+plt.figure(clustering_degree)
+plt.xlabel('Degree centrality')
+plt.ylabel('Clustering coefficient')
+plt.title('Clustering coefficient against degree centrality')
+plt.legend(bbox_to_anchor=(1,1))
+plt.tight_layout()
+plt.savefig(f"images/degree-clustering.png",bbox_inches='tight')
+
+plt.figure(degree_currentFlow)
+plt.xlabel('Current flow betweenness')
+plt.ylabel('Degree centrality')
+plt.title('Degree centrality against current flow betweenness')
+plt.legend(bbox_to_anchor=(1,1))
+plt.tight_layout()
+plt.savefig(f"images/CFbetweenness-degree.png",bbox_inches='tight')
+
 """Uncomment to decorate 3D plot"""
 # plt.figure(degree)
-# ax.set_xlabel('Mu')
-# ax.set_ylabel('Average degree (as LFR parameter)')
-# ax.set_zlabel('Degree centrality')
-# pickle.dump((degree, ax), open('images/3D-degreeCentrality.pickle', 'wb'))
+# ax_degree.set_xlabel('Mu')
+# ax_degree.set_ylabel('Average degree (as LFR parameter)')
+# ax_degree.set_zlabel('Degree centrality')
+# pickle.dump((degree, ax_degree), open('images/3D-degreeCentrality.pickle', 'wb'))
+# plt.savefig(f"images/3D-degreeCentrality.png")
+
+# plt.figure(betweenness)
+# ax_betweenness.set_xlabel('Mu')
+# ax_betweenness.set_ylabel('Average degree (as LFR parameter)')
+# ax_betweenness.set_zlabel('Betweenness centrality')
+# pickle.dump((betweenness, ax_betweenness), open('images/3D-betweennessCentrality.pickle', 'wb'))
+# plt.savefig(f"images/3D-betweennessCentrality.png")
+
+# plt.figure(CFbetweenness)
+# ax_CFbetweenness.set_xlabel('Mu')
+# ax_CFbetweenness.set_ylabel('Average degree (as LFR parameter)')
+# ax_CFbetweenness.set_zlabel('Current flow betweenness centrality')
+# pickle.dump((CFbetweenness, ax_CFbetweenness), open('images/3D-CFbetweennessCentrality.pickle', 'wb'))
+# plt.savefig(f"images/3D-CFbetweennessCentrality.png")
+
+# plt.figure(closeness)
+# ax_closeness.set_xlabel('Mu')
+# ax_closeness.set_ylabel('Average degree (as LFR parameter)')
+# ax_closeness.set_zlabel('Closeness centrality')
+# pickle.dump((closeness, ax_closeness), open('images/3D-closenessCentrality.pickle', 'wb'))
+# plt.savefig(f"images/3D-closenessCentrality.png")
 
 # plt.figure(clustering)
-# ax.set_xlabel('Mu')
-# ax.set_ylabel('Average degree (as LFR parameter)')
-# ax.set_zlabel('Clustering coefficient')
-# pickle.dump((clustering, ax), open('images/3D-clusteringCoefficient.pickle', 'wb'))
+# ax_clustering.set_xlabel('Mu')
+# ax_clustering.set_ylabel('Average degree (as LFR parameter)')
+# ax_clustering.set_zlabel('Clustering coefficient')
+# pickle.dump((clustering, ax_clustering), open('images/3D-clusteringCoefficient.pickle', 'wb'))
+# plt.savefig(f"images/3D-clusteringCoefficient.png")
 """"Uncomment until here"""
-
-# multiple linear regression
-model_degree = smf.ols(f'degreeCentrality ~  mu + average_degree', data=data).fit()
-print(model_degree.summary())
-
-model_clustering = smf.ols(f'clusteringCoefficient ~  mu + average_degree', data=data).fit()
-print(model_clustering.summary())
