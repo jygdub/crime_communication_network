acac48a4d3aa2e17bb317b4e7ebe2cec7bc252dd
jygdub <jade.dubbeld@gmail.com>
2024-02-13 13:50:09 +0100

    run multiple linear regression on parameters to predict measure

---
+++ LFR-networks/GLM_LFR.py
@@ -0,0 +1,29 @@
+"""
+Script to run multiple linear regression on LFR benchmark graph parameters vs. various network measures.
+
+Written by Jade Dubbeld
+12/02/2024
+"""
+
+import pandas as pd
+import statsmodels.formula.api as smf 
+
+data = pd.read_csv('data/all-measures.tsv',sep='\t')
+print(data)
+
+
+# multiple linear regression
+model_degree = smf.ols(f'degreeCentrality ~  mu + average_degree', data=data).fit()
+print(model_degree.summary())
+
+model_betweenness = smf.ols(f'betweennessCentrality ~  mu + average_degree', data=data).fit()
+print(model_betweenness.summary())
+
+model_CFbetweenness = smf.ols(f'currentFlowBetweenness ~  mu + average_degree', data=data).fit()
+print(model_CFbetweenness.summary())
+
+model_closeness = smf.ols(f'closenessCentrality ~  mu + average_degree', data=data).fit()
+print(model_closeness.summary())
+
+model_clustering = smf.ols(f'clusteringCoefficient ~  mu + average_degree', data=data).fit()
+print(model_clustering.summary())
