a5e27e6e45956fb590f2e6f7a087831e594a3495
jygdub <jade.dubbeld@gmail.com>
2024-02-22 11:44:00 +0100

    adjust for single test run

--- LFR-networks/sim_speedup_LFR.py
+++ LFR-networks/sim_speedup_LFR.py
@@ -9,14 +9,13 @@ import networkx as nx, random, numpy as np, matplotlib.pyplot as plt, pickle, ti
 
 from LFR_network import init, hamming_distance, simulate
 
-exp_degree = 3.0
-exp_community = 1.5
-
 alpha = 1.0
 beta = 0.0
 
+# # retrieve filenames from directory
 # listFileNames = sorted(glob.glob(f'graphs/first-generation/tau1={exp_degree}-tau2={exp_community}-*.pickle'))
 
+# # test files
 # filename = 'graphs/test100-tau1=3.0-tau2=1.5-mu=0.1-avg_deg=5-min_comm=5-seed=0.pickle'
 # filename = 'graphs/test10-tau1=3.0-tau2=1.5-mu=0.3-avg_deg=2-min_comm=2-seed=0.pickle'
 filename = 'graphs/first-generation/tau1=3.0-tau2=1.5-mu=0.25-avg_deg=10-min_comm=10-seed=12.pickle'
