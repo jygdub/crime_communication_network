ff0dabf0bc2b3e93ab34fdff987cae34be013da4
jygdub <jade.dubbeld@gmail.com>
2024-03-18 17:03:43 +0100

    save results to DataFrames

--- graph-Atlas/simulate_Atlas.py
+++ graph-Atlas/simulate_Atlas.py
@@ -1,5 +1,6 @@
 """
 Script to run communication dynamics on networkx's Graph Atlas networks.
+- Saving intermediate results
 
 Written by Jade Dubbeld
 12/03/2024
@@ -23,116 +24,38 @@ def visualize(G):
     nx.draw(G, pos, labels=nx.get_node_attributes(G,'state'))
     plt.show()
 
-
-def tolerant_mean(arrs):
-    """
-    Function to compute mean of curves with differing lengths
-
-    Parameters:
-    - arrs: nested list with sublists of different lengths
-
-    Returns:
-    - arr.mean: computed mean over all sublists
-    - arr.std: computed standard deviation over all sublists
-    """
-    lens = [len(i) for i in arrs]
-    arr = np.ma.empty((np.max(lens),len(arrs)))
-    arr.mask = True
-    for idx, l in enumerate(arrs):
-        arr[:len(l),idx] = l
-    return arr.mean(axis = -1), arr.std(axis=-1)
-
 listFileNames = sorted(glob.glob(f'graphs/*.pickle'))
 df = pd.read_csv('data-GraphAtlas.tsv',sep='\t')
 
-
-# for j,file in enumerate(listFileNames):
 for j in tqdm(range(len(df))):
-    # name = file[file.index('graphs/') + len('graphs/') + 0: file.index('.pickle')]
+
+    # generate filename
     name = 'G' + str(df['index'].iloc[j])
     file = f'graphs/{name}.pickle'
 
+    # load graph from file
     G = pickle.load(open(file,'rb'))
-
-    # fig_scatter = plt.figure()
-    # fig_converge = plt.figure()
-    # fig_mean = plt.figure()
 
     total_messages = []
     allDifferences = []
 
+    # simulate 100 iterations
     for i in range(100):
-
-        # fig_states = plt.figure()
-
         states = []
-
         G_init = init(G)
 
-        # TODO: save ALL data in DataFrame instead of plotting
         M, meanStringDifference,states_trajectory = simulate(G_init)
 
+        # save state diversity to DataFrame
         df_states = pd.DataFrame(states_trajectory)
         df_states.to_csv(f'results/states-{name}-run{i}.tsv',sep='\t',index=False)
-
-        # for k,v in states_trajectory.items():
-        #     plt.figure(fig_states)
-        #     plt.plot(np.arange(0,len(meanStringDifference)), v, label=k)
-        
-        # plt.xlabel("Total messages sent", fontsize=12)
-        # plt.ylabel("Frequency", fontsize=12)
-        # plt.xticks(fontsize=12)
-        # plt.yticks(fontsize=12)
-        # plt.title(f"States trajectory - Graph Atlas {name} (n={df.nodes.iloc[j]};run={i})")
-        # plt.legend(bbox_to_anchor=(1,1))
-        # plt.savefig(f"images/simulation-results/states-{name}-run{i}.png",bbox_inches='tight')
-        # plt.close(fig_states)    
 
         total_messages.append(M)
         allDifferences.append(list(meanStringDifference))
 
-
+    # save convergence rate to DataFrame
     df_convergence = pd.DataFrame(
         {'nMessages': total_messages,
          'meanHammingDist': allDifferences}
     )
-    
     df_convergence.to_csv(f'results/convergence-{name}.tsv',sep='\t',index=False)
-
-
-    #     plt.figure(fig_converge)
-    #     plt.plot(np.arange(0,len(meanStringDifference)), np.reshape(meanStringDifference,(-1,1)),label=f'Run {i}')
-
-
-    # plt.figure(fig_scatter)
-    # plt.scatter(range(100),total_messages)
-    # plt.xlabel("Networks", fontsize=12)
-    # plt.ylabel("Total messages sent", fontsize=12)
-    # plt.xticks(fontsize=12)
-    # plt.yticks(fontsize=12)
-    # plt.title(f"Consensus distribution (N=100) - Graph Atlas {name} (n={df.nodes.iloc[j]})")
-    # plt.savefig(f"images/simulation-results/scatter-{name}.png",bbox_inches='tight')
-    # plt.close(fig_scatter)
-
-    # plt.figure(fig_converge)
-    # plt.xlabel("Total messages sent", fontsize=12)
-    # plt.ylabel("Average string difference (Hamming distance)", fontsize=12)
-    # plt.xticks(fontsize=12)
-    # plt.yticks(fontsize=12)
-    # plt.title(f"Convergence rates (N=100) - Graph Atlas {name} (n={df.nodes.iloc[j]})")
-    # # plt.legend(bbox_to_anchor=(1,1))
-    # plt.savefig(f"images/simulation-results/convergence-{name}.png", bbox_inches='tight')
-    # plt.close(fig_converge)
-
-    # # mean convergence plot over all simulations
-    # plt.figure(fig_mean)
-    # y, error = tolerant_mean(allDifferences)
-    # plt.plot(np.arange(len(y))+1, y)
-    # plt.fill_between(np.arange(len(y))+1, y-error, y+error, alpha=0.5)
-    # plt.xlabel("Total messages sent", fontsize=14)
-    # plt.ylabel("Average string difference (Hamming distance)", fontsize=14)
-    # plt.xticks(fontsize=14)
-    # plt.yticks(fontsize=14)
-    # plt.title(f"Mean convergence (N=100) - Graph Atlas {name} (n={df.nodes.iloc[j]})")
-    # plt.savefig(f"images/simulation-results/mean-convergence-{name}.png",bbox_inches='tight')
-    # plt.close(fig_mean)
