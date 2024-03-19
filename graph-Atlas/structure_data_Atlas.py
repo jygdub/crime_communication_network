881c036b8f0232a5157fab2a896a6fa7a5f5d2db
jygdub <jade.dubbeld@gmail.com>
2024-03-19 12:27:33 +0100

    add other metrics

--- graph-Atlas/structure_data_Atlas.py
+++ graph-Atlas/structure_data_Atlas.py
@@ -10,9 +10,20 @@ from tqdm import tqdm
 import numpy as np, pandas as pd
 
 
-metrics = pd.read_csv('data-GraphAtlas.tsv', usecols=['index','nodes','globalEff','localEff'], sep='\t')
+metrics = pd.read_csv('data-GraphAtlas.tsv', sep='\t')
+print(metrics)
 
-complete = pd.DataFrame(data=None, index=np.arange(len(metrics)*100), columns=['index','nodes','globalEff','localEff','nMessages'])
+complete = pd.DataFrame(data=None, index=np.arange(len(metrics)*100), 
+                        columns=['index',
+                                 'nodes',
+                                 'degree',
+                                 'betweenness',
+                                #  'CFbetweennesss',
+                                 'closeness',
+                                 'clustering',
+                                 'globalEff',
+                                 'localEff',
+                                 'nMessages'])
 
 for idx in tqdm(range(len(metrics))):
 
@@ -26,6 +37,11 @@ for idx in tqdm(range(len(metrics))):
 
     complete['index'].iloc[n100:n100+100] = metrics['index'].iloc[idx]
     complete['nodes'].iloc[n100:n100+100] = metrics['nodes'].iloc[idx]
+    complete['degree'].iloc[n100:n100+100] = metrics['degree'].iloc[idx]
+    complete['betweenness'].iloc[n100:n100+100] = metrics['betweenness'].iloc[idx]
+    # complete['CFbetweenness'].iloc[n100:n100+100] = metrics['CFbetweenness'].iloc[idx]
+    complete['closeness'].iloc[n100:n100+100] = metrics['closeness'].iloc[idx]
+    complete['clustering'].iloc[n100:n100+100] = metrics['clustering'].iloc[idx]
     complete['globalEff'].iloc[n100:n100+100] = metrics['globalEff'].iloc[idx]
     complete['localEff'].iloc[n100:n100+100] = metrics['localEff'].iloc[idx]
     complete['nMessages'].iloc[n100:n100+100] = convergence['nMessages']
