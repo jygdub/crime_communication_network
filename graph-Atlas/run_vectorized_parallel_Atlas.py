992c04cb6501a2eff2a43792bccf22ceb32f20eb
jygdub <jade.dubbeld@gmail.com>
2024-03-19 20:27:43 +0100

    implementated parallel simulation of vectorized dynamics

---
+++ graph-Atlas/run_vectorized_parallel_Atlas.py
@@ -0,0 +1,58 @@
+"""
+Script to run communication dynamics on networkx's Graph Atlas networks.
+- Saving intermediate results
+- Using vectorized implementation
+
+Written by Jade Dubbeld
+19/03/2024
+"""
+
+from dynamics_vectorized_Atlas import simulate
+
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from itertools import product
+import multiprocessing as mp, numpy as np, pandas as pd, glob, pickle
+
+def run(x):
+    # print(mp.current_process())
+
+    graph, i = x
+
+    alpha = 1.0
+    beta = 0.0
+
+    totalMessages, meanHammingDistance, statesTrajectories, graph = simulate(graph=graph, alpha=alpha, beta=beta)
+    
+    return totalMessages, meanHammingDistance, statesTrajectories, graph, i
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('data-GraphAtlas.tsv',sep='\t')
+
+    graphs = []
+
+    for j in range(len(df)-900):
+        # generate filename
+        name = 'G' + str(df['index'].iloc[j])
+        # file = f'graphs/{name}.pickle'
+
+        graphs.append(name)
+
+    print(graphs)
+
+    with mp.Pool(processes=mp.cpu_count()-1) as p:
+        for result in p.imap_unordered(run, product(graphs,np.arange(10))):
+            M, meanHammingDistance, states_trajectory, graph, i = result
+
+            df_states = pd.DataFrame(states_trajectory)
+            df_states.to_csv(f'test/states-{graph}-run{i}.tsv',sep='\t',index=False)
+
+            df_convergence = pd.DataFrame(
+                {'graph': graph,
+                'nMessages': M,
+                'meanHammingDist': [meanHammingDistance]}
+            )
+
+            df_convergence.to_csv(f'test/convergence-{graph}-run{i}.tsv',sep='\t',index=False)
