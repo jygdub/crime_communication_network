eb621dd5626e7883298b29ec0c3194aae49f2489
jygdub <jade.dubbeld@gmail.com>
2024-03-27 11:55:53 +0100

    bash script to run dynamics for Atlas networks on Snellius

---
+++ graph-Atlas/bash_Atlas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name="newt"
+#SBATCH --partition genoa
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time 1:00:00 #runtime
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=192
+
+cp $HOME/Graph-Atlas/data-GraphAtlas.tsv /scratch-shared/Graph-Atlas
+cp -r $HOME/Graph-Atlas/graphs /scratch-shared/Graph-Atlas
+cp $HOME/Graph-Atlas/run_vectorized_parallel_Atlas.py /scratch-shared/Graph-Atlas
+cp $HOME/Graph-Atlas/dynamics_vectorized_Atlas.py /scratch-shared/Graph-Atlas
+
+cd /scratch-shared/Graph-Atlas
+module load 2023 
+module load Python/3.11.3-GCCcore-12.3.0 # python version
+
+# load required packages not available on Snellius
+# pip install --user itertools
+pip install --user numpy
+pip install --user pandas
+
+python3 run_vectorized_parallel_Atlas.py # run script
+cp results/test/* $HOME/Graph-Atlas/results/test # copy result files in pre-existing 
