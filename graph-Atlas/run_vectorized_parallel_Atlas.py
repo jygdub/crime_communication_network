268156238ae8e489ff57c86b17e839d1a3abdee8
jygdub <jade.dubbeld@gmail.com>
2024-03-27 10:28:48 +0100

    adjust to write output to correct folder locally

--- graph-Atlas/run_vectorized_parallel_Atlas.py
+++ graph-Atlas/run_vectorized_parallel_Atlas.py
@@ -20,7 +20,7 @@ def run(x):
     graph, i = x
 
     alpha = 1.0
-    beta = 0.0
+    beta = 0.50
 
     totalMessages, meanHammingDistance, statesTrajectories, graph = simulate(graph=graph, alpha=alpha, beta=beta)
     
@@ -29,23 +29,25 @@ def run(x):
 
 if __name__ == "__main__":
 
+    path = "results/alpha1_00-beta0_50"
+
     df = pd.read_csv('data-GraphAtlas.tsv',sep='\t')
 
     graphs = []
 
-    for j in range(len(df)-900):
+    for j in range(len(df)):
 
         # generate graph ID
         name = 'G' + str(df['index'].iloc[j])
 
         graphs.append(name)
 
-    with mp.Pool(processes=mp.cpu_count()-1) as p: # NOTE: remove -1 from cpu_count for simulation on Snellius
-        for result in p.imap_unordered(run, product(graphs,np.arange(10))):
+    with mp.Pool(processes=mp.cpu_count()) as p: # NOTE: remove -1 from cpu_count for simulation on Snellius
+        for result in tqdm(p.imap_unordered(run, product(graphs,np.arange(100)))):
             M, meanHammingDistance, states_trajectory, graph, i = result
 
             df_states = pd.DataFrame(states_trajectory)
-            df_states.to_csv(f'test/states-{graph}-run{i}.tsv',sep='\t',index=False)
+            df_states.to_csv(f'{path}/states-{graph}-run{i}.tsv',sep='\t',index=False)
 
             df_convergence = pd.DataFrame(
                 {'graph': graph,
@@ -53,4 +55,4 @@ if __name__ == "__main__":
                 'meanHammingDist': [meanHammingDistance]}
             )
 
-            df_convergence.to_csv(f'test/convergence-{graph}-run{i}.tsv',sep='\t',index=False)
+            df_convergence.to_csv(f'{path}/convergence-{graph}-run{i}.tsv',sep='\t',index=False)
