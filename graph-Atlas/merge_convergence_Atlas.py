339e3862e3f39ba47824933c2e836e10b6c449f6
jygdub <jade.dubbeld@gmail.com>
2024-04-18 01:44:27 +0200

    merge new convergence results

--- graph-Atlas/merge_convergence_Atlas.py
+++ graph-Atlas/merge_convergence_Atlas.py
@@ -10,7 +10,9 @@ Written by Jade Dubbeld
 import pandas as pd
 from tqdm import tqdm
 
-df = pd.read_csv('data-GraphAtlas.tsv',sep='\t')
+# df = pd.read_csv('data-GraphAtlas.tsv',sep='\t')
+df = pd.read_csv('data/data-GraphAtlas.tsv',sep='\t')
+
 
 graphs = []
 
@@ -26,7 +28,10 @@ for graph in tqdm(graphs):
     data = pd.DataFrame(index=range(100),columns=['nMessages','meanHammingDist'])
 
     for i in range(100):
-        run = pd.read_csv(f'results/alpha1_00-beta0_50/convergence-{graph}-run{i}.tsv', sep='\t', usecols=['nMessages','meanHammingDist'])
+        run = pd.read_csv(f'results/alpha0_50-beta0_25/convergence-{graph}-run{i}.tsv', sep='\t', usecols=['nMessages','meanHammingDist'])
+        # run = pd.read_csv(f'/Volumes/Disk-Jade/CLS-data/alpha0_75-beta0_50/convergence-{graph}-run{i}.tsv', sep='\t', usecols=['nMessages','meanHammingDist'])
+
         data.iloc[i] = run.iloc[0]
 
-    data.to_csv(f'results/alpha1_00-beta0_50/merged/convergence-{graph}.tsv', sep='\t',index=False)
+    data.to_csv(f'results/alpha0_50-beta0_25/merged/convergence-{graph}.tsv', sep='\t',index=False)
+    # data.to_csv(f'/Volumes/Disk-Jade/CLS-data/alpha0_75-beta0_50/convergence-{graph}.tsv', sep='\t',index=False)
