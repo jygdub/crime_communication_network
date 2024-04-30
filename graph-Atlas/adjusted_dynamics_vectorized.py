0fe40959f2573c4eca3e822d58b0194439505de6
jygdub <jade.dubbeld@gmail.com>
2024-04-30 22:49:08 +0200

    predefine neighbors

--- graph-Atlas/adjusted_dynamics_vectorized.py
+++ graph-Atlas/adjusted_dynamics_vectorized.py
@@ -96,45 +96,6 @@ def hamming_vector(states: np.ndarray, agents: slice) -> np.ndarray:
 
     return (states[agents,np.newaxis] != states[np.newaxis,:]).mean(-1)
 
-def count_states(states: np.ndarray, states_trajectory: dict) -> dict:
-
-    counts = {'000': 0,
-              '001': 0,
-              '010': 0,
-              '011': 0,
-              '100': 0,
-              '101': 0,
-              '110': 0,
-              '111': 0}
-
-    for s in states:
-        if (s==np.array([0,0,0])).all():
-            counts['000'] += 1
-        elif (s==np.array([0,0,1])).all():
-            counts['001'] += 1
-        elif (s==np.array([0,1,0])).all():
-            counts['010'] += 1
-        elif (s==np.array([0,1,1])).all():
-            counts['011'] += 1
-        elif (s==np.array([1,0,0])).all():
-            counts['100'] += 1
-        elif (s==np.array([1,0,1])).all():
-            counts['101'] += 1
-        elif (s==np.array([1,1,0])).all():
-            counts['110'] += 1
-        elif (s==np.array([1,1,1])).all():
-            counts['111'] += 1
-
-    for state in states_trajectory.keys():
-
-        if counts[state] != 0:
-            states_trajectory[state].append(counts[state])
-        else:
-            states_trajectory[state].append(0)
- 
-    return states_trajectory
-
-
 def simulate(graph: str, alpha: float = 1.0, beta: float = 0.0) -> int | list:
     """
     Function to run simulation of communication dynamics on network.
@@ -160,10 +121,20 @@ def simulate(graph: str, alpha: float = 1.0, beta: float = 0.0) -> int | list:
 
     nodes = list(G.nodes())
 
+    # initialize neighbor dictionary
+    dictNeighbors = {key: [] for key in nodes}
+
+    # find neighbors of all nodes and store in dictionary
+    for node in nodes:
+        neighbors = G.neighbors(node)
+        
+        for nb in neighbors:
+            dictNeighbors[node].append(nb)
+
     # converge when all nodes agree on state
     while (hammingDistance.mean() != 0.0):
         source = np.random.choice(nodes)
-        destination = np.random.choice(list(G.neighbors(source))) # TODO: pre-define neighbors
+        destination = np.random.choice(dictNeighbors[source])
 
         states = message_update(states, source, destination, alpha=alpha, beta=beta)
 
