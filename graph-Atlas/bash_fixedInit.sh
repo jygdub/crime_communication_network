62a5e8793e4db51c102f950542b887c52d1da0e6
jygdub <jade.dubbeld@gmail.com>
2024-04-30 22:53:06 +0200

    implement simulation with fixed state initialization (run on Snellius)

--- graph-Atlas/bash_fixedInit.sh
+++ graph-Atlas/bash_fixedInit.sh
@@ -10,8 +10,8 @@
 
 cp $HOME/Graph-Atlas/data-GraphAtlas.tsv /scratch-shared/Graph-Atlas
 cp -r $HOME/Graph-Atlas/graphs /scratch-shared/Graph-Atlas
-cp $HOME/Graph-Atlas/adjusted_parallel_run.py /scratch-shared/Graph-Atlas
-cp $HOME/Graph-Atlas/adjusted_dynamics_vectorized.py /scratch-shared/Graph-Atlas
+cp $HOME/Graph-Atlas/run_fixedInit_dynamics_parallel.py /scratch-shared/Graph-Atlas
+cp $HOME/Graph-Atlas/fixedInit_dynamics_vectorized.py /scratch-shared/Graph-Atlas
 
 cd /scratch-shared/Graph-Atlas
 module load 2023 
@@ -22,4 +22,4 @@ module load Python/3.11.3-GCCcore-12.3.0 # python version
 pip install --user numpy
 pip install --user pandas
 
-python3 adjusted_parallel_run.py # run script
+python3 run_fixedInit_dynamics_parallel.py # run script
